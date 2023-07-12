204ddbeb2d6ff5eb34f07b884c9cd8fa2bd7415a
cedric.anaky.ext <heinzcodeur@gmail.com>
2023-07-12 11:47:29 +0200

    second

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -6,6 +9,20 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit{
+
+   pokemonList: Pokemon[];
+   pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute){}
+
+   ngOnInit(){
+      this.pokemonList = POKEMONS;
+      const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+      if(pokemonId){
+        this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId );
+      }
+
+   }
 
 }
